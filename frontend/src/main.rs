57c5b872f5037809de1abea1ea05012539dacd4c
hayatroid <hayatroid@gmail.com>
2024-12-28 01:24:38 +0900

    feat: chat client with yew

--- frontend/src/main.rs
+++ frontend/src/main.rs
@@ -1,3 +1,53 @@
+use web_sys::HtmlTextAreaElement;
+use yew::prelude::*;
+use yew_hooks::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    yew::Renderer::<App>::new().render();
+}
+
+#[function_component]
+fn App() -> Html {
+    let ws = use_websocket("ws://127.0.0.1:7878".to_string());
+    let msg = use_state(|| "".to_string());
+    let messages = use_list(vec![]);
+
+    {
+        let ws = ws.clone();
+        let messages = messages.clone();
+        use_effect_with(ws.message, move |msg| {
+            if let Some(msg) = &**msg {
+                messages.push(msg.clone());
+            }
+        });
+    }
+
+    let onchange = {
+        let msg = msg.clone();
+        Callback::from(move |e: Event| {
+            let textarea = e.target_dyn_into::<HtmlTextAreaElement>().unwrap();
+            msg.set(textarea.value());
+        })
+    };
+
+    let onclick = {
+        let ws = ws.clone();
+        let msg = msg.clone();
+        Callback::from(move |_| {
+            ws.send((*msg).clone());
+            msg.set("".to_string());
+        })
+    };
+
+    html! {
+        <>
+            {
+                for messages.current().iter().map(|msg| {
+                    html! { <p>{ msg }</p> }
+                })
+            }
+            <textarea { onchange } value={ (*msg).clone() }></textarea>
+            <button { onclick } disabled={ *ws.ready_state != UseWebSocketReadyState::Open }>{ "Send" }</button>
+        </>
+    }
 }
